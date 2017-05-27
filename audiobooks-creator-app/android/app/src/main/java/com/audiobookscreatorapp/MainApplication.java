1c796b2ab36f4d6d4cd356d7e335ad0296422e5d
Eugene Potapenko <eugenepotapenko@iMac-Eugene.local>
2017-05-27 13:11:33 +0200

    installed native components

--- audiobooks-creator-app/android/app/src/main/java/com/audiobookscreatorapp/MainApplication.java
+++ audiobooks-creator-app/android/app/src/main/java/com/audiobookscreatorapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.audiobookscreatorapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new ReactNativeAudioPackage(),
             new WebViewBridgePackage(),
             new VectorIconsPackage()
