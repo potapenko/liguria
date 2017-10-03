ef928bafb995959bc23a298e55927cc172a2e6a3
eugene_potapenko <eugene.john.potapenko@gmail.com>
2017-10-03 14:08:50 +0200

    migrated to reagent 0.7.0

--- liguria/project.clj
+++ liguria/project.clj
@@ -1,22 +1,23 @@
 (defproject liguria "0.1.0-SNAPSHOT"
-            :description "FIXME: write description"
-            :url "http://example.com/FIXME"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                           [org.clojure/clojurescript "1.9.542"]
-                           [reagent "0.6.1" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
-                           [re-frame "0.9.2"]
-                           [com.cognitect/transit-cljs "0.8.239"]
-                           [camel-snake-kebab "0.4.0"]
-                           [mount "0.1.11"]
-                           [org.clojure/core.async "0.3.443"]
-                           [thi.ng/color "1.2.0"]
-                           [thi.ng/math "0.2.1"]
-                           [natal-shell "0.5.1"]]
-            :plugins [[lein-cljsbuild "1.1.4"]
-                      [lein-figwheel "0.5.10"]]
-            :clean-targets ["target/" "index.ios.js" "index.android.js" #_($PLATFORM_CLEAN$)]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/clojurescript "1.9.542"]
+                 [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom
+                                               cljsjs/react-dom-server cljsjs/create-react-class]]
+                 [re-frame "0.9.2"]
+                 [com.cognitect/transit-cljs "0.8.239"]
+                 [camel-snake-kebab "0.4.0"]
+                 [mount "0.1.11"]
+                 [org.clojure/core.async "0.3.443"]
+                 [thi.ng/color "1.2.0"]
+                 [thi.ng/math "0.2.1"]
+                 [natal-shell "0.5.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]
+            [lein-figwheel "0.5.10"]]
+  :clean-targets ["target/" "index.ios.js" "index.android.js" #_($PLATFORM_CLEAN$)]
   :aliases {"prod-build" ^{:doc "Recompile code with prod profile."}
             ["do" "clean"
              ["with-profile" "prod" "cljsbuild" "once"]]}
@@ -60,5 +61,5 @@
                                                          :optimizations      :simple
                                                          :closure-defines    {"goog.DEBUG" false}}}
                                          #_($PROD_PROFILES$)]}}})
-                                                  
-                      
+
+
